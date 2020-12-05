1534d42f807d14a474880e76ba67aa4eff8aca29
CJ Gonzalez <rkmaster07@gmail.com>
2020-12-05 17:13:19 -0600

    Convert login screen to use viewbinding

--- app/src/main/java/com/cjapps/autonomic/login/LoginFragment.kt
+++ app/src/main/java/com/cjapps/autonomic/login/LoginFragment.kt
@@ -8,21 +8,18 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.cjapps.autonomic.R
+import com.cjapps.autonomic.databinding.FragLoginBinding
 import com.cjapps.utility.livedata.EventObserver
+import com.cjapps.utility.viewbinding.viewBindingLifecycle
 import com.google.android.material.snackbar.Snackbar
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.frag_login.*
 import timber.log.Timber
 import javax.inject.Inject
 
-/**
- * Created by cjgonz on 2019-12-30.
- */
 class LoginFragment: DaggerFragment(), IHostActivityResultListener {
     companion object {
         private const val SPOTIFY_LOGIN_REQUEST_CODE = 3487
@@ -32,6 +29,7 @@ class LoginFragment: DaggerFragment(), IHostActivityResultListener {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val loginViewModel by viewModels<LoginViewModel> { viewModelFactory }
+    private val viewBinding by viewBindingLifecycle { FragLoginBinding.bind(requireView()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,31 +50,31 @@ class LoginFragment: DaggerFragment(), IHostActivityResultListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        login_get_started_button.setOnClickListener { loginViewModel.loginButtonClicked() }
+        viewBinding.loginGetStartedButton.setOnClickListener { loginViewModel.loginButtonClicked() }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        loginViewModel.isLoadingLiveData.observe(viewLifecycleOwner, Observer { isLoading ->
+        loginViewModel.isLoadingLiveData.observe(viewLifecycleOwner, { isLoading ->
             if (isLoading) {
-                login_get_started_button.visibility = View.GONE
-                login_progress_loading_indicator.visibility = View.VISIBLE
+                viewBinding.loginGetStartedButton.visibility = View.GONE
+                viewBinding.loginProgressLoadingIndicator.visibility = View.VISIBLE
             } else {
-                login_get_started_button.visibility = View.VISIBLE
-                login_progress_loading_indicator.visibility = View.GONE
+                viewBinding.loginGetStartedButton.visibility = View.VISIBLE
+                viewBinding.loginProgressLoadingIndicator.visibility = View.GONE
             }
         })
         loginViewModel.launchAuthenticationLiveData.observe(viewLifecycleOwner, EventObserver {
             AuthenticationClient.openLoginActivity(activity, SPOTIFY_LOGIN_REQUEST_CODE, it)
         })
         loginViewModel.errorEventLiveData.observe(viewLifecycleOwner, EventObserver {
-            Snackbar.make(login_coordinator, it, Snackbar.LENGTH_LONG).show()
+            Snackbar.make(viewBinding.loginCoordinator, it, Snackbar.LENGTH_LONG).show()
         })
         loginViewModel.snackBarEventLiveData.observe(viewLifecycleOwner, EventObserver {
-            Snackbar.make(login_coordinator, it, Snackbar.LENGTH_LONG).show()
+            Snackbar.make(viewBinding.loginCoordinator, it, Snackbar.LENGTH_LONG).show()
         })
         loginViewModel.loginCompleteEventLiveData.observe(viewLifecycleOwner, EventObserver {
-            Snackbar.make(login_coordinator, "Logged In!", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(viewBinding.loginCoordinator, "Logged In!", Snackbar.LENGTH_LONG).show()
             findNavController().popBackStack()
         })
     }
